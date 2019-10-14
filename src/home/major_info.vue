5dd8bab4fbc637baca6c25a79c487ac35384adb6
chenglla <1225883052@qq.com>
2019-10-14 19:30:06 +0800

    职业详情更新

--- src/home/major_info.vue
+++ src/home/major_info.vue
@@ -1,32 +1,51 @@
 <template>
   <div>
-    <!--<h3 class="major_info_h3">职业详解</h3>-->
-    <!--<div v-for="item in major_content" :key="item">-->
-      <!--&lt;!&ndash;<div v-html="item.info">{{item.info}}</div>&ndash;&gt;-->
-      <!--<div>{{item.info}}</div>-->
-    <!--</div>-->
     <div class="serviceInfo" ref="serviceInfo">
       <div>
         <div class="one-title">
-          <span class="one-title__icon"></span>
+          <div class="return__icon" @click="gotoPage">
+            <i class="iconfont iconllfanhui4"></i>
+          </div>
+<!--          <span class="one-title__icon"></span>-->
           <span>职业详解</span>
           <span class="one-title__bacicon iconfont icon-jiantou">职业详解</span>
         </div>
-        <!--<div class="one-item">-->
-          <!--<p class="one__p">该套系统实现师生对饭菜质量及餐饮服务的评价反馈，满足师生与校园餐饮管理的有效互动，辅助校园餐厅提高服务质量、改善菜品结构搭配，力求为学校广大师生群体提供更加健康、养生、合理的餐饮服务<strong>注意：使用该系统前请关注“中科智禾”微信公众号,选择“智禾教育”模块，点击“食堂系统”进行使用</strong></p>-->
-        <!--</div>-->
-
-        <div class="one-item" v-for="item in major_content" :key="item" v-if="item.info!==''">
-          <!--<div class="one-item__header">功能介绍</div>-->
-          <div class="one-step" v-html="item.info" >
-            <div class="one-step__item">
-              <span>{{item.info}}</span>
+        <div class="one-item" v-for="item in major_content" :key="item" v-if="item.subClassTitleAndContent.length > 0">
+          <h3 class="one-item-title">{{item.classTitle}}</h3>
+<!--          <div class="one-item__content" v-for="(value,key,index) in item.subClassTitleAndContent[0]">-->
+<!--&lt;!&ndash;            {{item.subClassTitleAndContent}}&ndash;&gt;-->
+<!--            {{value}}&#45;&#45;{{key}}-&#45;&#45;{{index}} 哈哈哈哈-->
+<!--          </div>-->
+          <div class="one-item__content" v-for="(project, index) in item.subClassTitleAndContent" :key="index">
+            <div v-for="(value,key,index) in project" :key="index">
+<!--              {{key}}-&#45;&#45;{{value}}&#45;&#45;{{index}}-->
+              <div v-if="key === ''" v-html="value[0].info" class="one-item-subcontent"></div>
+              <div v-else>
+                <h4 class="one-item-subtitle">{{key}}</h4>
+                <div class="one-item-subcontent" v-html="value[0].info" v-if="value[0].info"></div>
+                <div class="one-item-subcontent" v-html="value[0].specName" v-else></div>
+              </div>
+              <div class="one-item-img" v-if="value[0].imgId"><img :src="'https://xz.chsi.com.cn/occupation/showlabelimgori.action?labelimgid='+value[0].imgId" alt="这是图片"/></div>
             </div>
           </div>
+<!--          <div class="one-step" v-html="item.info" >-->
+<!--            <div class="one-step__item">-->
+<!--              <span>{{item.info}}</span>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--          <div class="one-item-img" v-if="item.imgId"><img :src="'https://xz.chsi.com.cn/occupation/showlabelimgori.action?labelimgid='+item.imgId" alt="这是图片"/></div>-->
         </div>
+<!--        <h3>内容</h3>-->
+<!--        <span>{{major_content[0]}}</span>-->
+<!--        <span>{{major_content[1].info}}</span>-->
+<!--        <span>{{major_content[2].info}}</span>-->
+<!--        <span>{{major_content[3].info}}</span>-->
+<!--        <span>{{major_content[4].info}}</span>-->
       </div>
     </div>
-<!--    {{major_content}}-->
+<!--    哈哈哈：{{major_content}}-->
+<!--    <img :src="'https://xz.chsi.com.cn/occupation/showlabelimgori.action?labelimgid='+major_content[0].imgId"/>-->
+<!--    <img :src=`https://xz.chsi.com.cn/occupation/showlabelimgori.action?labelimgid=${major_content[0].imgId}`>-->
   </div>
 </template>
 <script>
@@ -36,6 +55,11 @@ export default {
     return {
       id: this.$route.query.id,
       major_content: []
+    }
+  },
+  computed: {
+    imgid () {
+      return this.major_content[0].imgId
     }
   },
   mounted () {
@@ -56,6 +80,10 @@ export default {
         this.major_content = response.data
         console.log(this.major_content)
       })
+    },
+    gotoPage () {
+      console.log('return')
+      this.$router.go(-1)
     }
   }
 }
@@ -83,6 +111,47 @@ export default {
   font-size: 18px;
   font-weight: bold;
 }
+.return__icon {
+  margin-left: 30px;
+  position: absolute;
+  height: 30px;
+}
+.iconfont {
+  position: absolute;
+  font-size: 24px;
+  margin: 0 10px;
+}
+.one-item-img {
+  /*width: 100px;*/
+  text-align: center;
+  height: 170px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+}
+.one-item-img img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+}
+.one-item-title {
+  margin: 5px 3px;
+  /*font-weight: normal;*/
+}
+.one-item-subtitle {
+  margin: 7px;
+}
+.one-item-subcontent {
+  /*text-indent: 2em;*/
+  line-height: 23px;
+  font-size: 14px;
+  color: #3c3c3c;
+  margin: 5px 1px 3px 10px;
+}
+/*p {*/
+/*  text-indent: 2em;*/
+/*}*/
 .one-title__subtitle {
   position: absolute;
   left: 50%;
@@ -111,7 +180,7 @@ export default {
 }
 .one-item{
   background: rgba(255,255,204, 0.1);
-  padding: 16px;
+  padding: 14px;
   margin: 20px 20px 16px;
   box-shadow: 0 6px 6px rgba(75, 92, 178, 0.1);
   border-radius: 4px;
@@ -119,7 +188,7 @@ export default {
   overflow: hidden;
 }
 .one-step {
-  margin-bottom: 20px;
+  margin-bottom: 5px;
 }
 .one__fee {
   margin-top: 10px;
@@ -148,15 +217,14 @@ export default {
   font-size: 15px;
   font-weight: bold;
 }
-.iconfont{
-  margin: 0 10px;
-}
-.text {
-  font-weight: bold;
-}
+/*.iconfont{*/
+/*  margin: 0 10px;*/
+/*}*/
+
 .one-step__item {
   text-align: left;
   padding-top: 10px;
+  /*padding-bottom: 10px;*/
 }
 .one-step__item {
   position: relative;
