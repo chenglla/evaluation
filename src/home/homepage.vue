bb414d84a9fb5b931a628d7dea705be6f065634a
chenglla <1225883052@qq.com>
2019-10-15 10:09:34 +0800

    导航调整

--- src/home/homepage.vue
+++ src/home/homepage.vue
@@ -5,12 +5,12 @@
       <tab v-model="index01" :line-width="1" bar-active-color="#668599">
 <!--        <tab-item >测评分类</tab-item>-->
         <tab-item selected @on-item-click="clickJob">职业探测</tab-item>
+        <tab-item @on-item-click="clickAttainment">基本素养</tab-item>
         <tab-item @on-item-click="clickAbility">能力测评</tab-item>
         <tab-item @on-item-click="clickAttitude">态度测试</tab-item>
         <tab-item @on-item-click="clickCharacter">性格特点</tab-item>
         <tab-item @on-item-click="clickCareer">生涯测试</tab-item>
         <tab-item @on-item-click="clickDecision">决策风格</tab-item>
-        <tab-item @on-item-click="clickAttainment">基本素养</tab-item>
         <tab-item @on-item-click="clickComprehensive">综合素养</tab-item>
         <tab-item @on-item-click="onItemClick">素养</tab-item>
       </tab>
@@ -20,7 +20,7 @@
         <div class="one-item" @click="gotopage">
           <div class="one-item__header">
             <div class="one-item_content_img">
-              <img src="../assets/5678.jpg" width="60">
+              <img src="../assets/5677.jpg" width="60">
             </div>
             <div class="one-item_content">
                <span>
@@ -33,36 +33,37 @@
         <div class="one-item" @click="gotopage">
           <div class="one-item__header">
             <div class="one-item_content_img">
-              <img src="../assets/5677.jpg" width="60">
+              <img src="../assets/5678.jpg" width="60">
             </div>
             <div class="one-item_content">
                <span>
-                左右脑偏好测试
+                霍兰德职业兴趣测试
                </span>
             </div>
           </div>
 <!--          <p class="one__p">右脑控制着自律神经与宇宙波动共振等，和潜意识有关。左脑被称为“语言脑”，控制语言能力，注重细节和推理。左大脑发达的人善于沟通和说服他人。如果你更偏好左脑，你可能在数学和逻辑领域表现很好。右脑是与创造性和灵活性相关的。右脑偏好的人更大胆直观，以独特的方式看待世界</p>-->
         </div>
+
+      </div>
+      <div v-if="showWhich === 'ability'" class="one-item_all">ability</div>
+      <div v-if="showWhich === 'attitude'" class="one-item_all">attitude</div>
+      <div v-if="showWhich === 'character'" class="one-item_all">character</div>
+      <div v-if="showWhich === 'career'" class="one-item_all">career</div>
+      <div v-if="showWhich === 'decision'" class="one-item_all">decision</div>
+      <div v-if="showWhich === 'attainment'" class="one-item_all">
         <div class="one-item" @click="gotopage">
           <div class="one-item__header">
             <div class="one-item_content_img">
               <img src="../assets/455.jpg" width="60">
             </div>
             <div class="one-item_content">
-                 抑郁症心理测试
+              抑郁症心理测试
             </div>
           </div>
-<!--          <p class="one__p">抑郁症又称抑郁障碍，以显著而持久的心境低落为主要临床特征，是心境障碍的主要类型。临床可见心境低落与其处境不相称，情绪的消沉可以从闷闷不乐到悲痛欲绝，自卑抑郁，甚至悲观厌世，可有自杀企图或行为；甚至发生木僵；部分病例有明显的焦虑和运动性激越；严重者可出现幻觉、妄想等精神病性症状。每次发作持续至少2周以上、长者甚或数年，多数病例有反复发作的倾向，每次发作大多数可以缓解，部分可有残留症状或转为慢性。</p>-->
+          <!--          <p class="one__p">抑郁症又称抑郁障碍，以显著而持久的心境低落为主要临床特征，是心境障碍的主要类型。临床可见心境低落与其处境不相称，情绪的消沉可以从闷闷不乐到悲痛欲绝，自卑抑郁，甚至悲观厌世，可有自杀企图或行为；甚至发生木僵；部分病例有明显的焦虑和运动性激越；严重者可出现幻觉、妄想等精神病性症状。每次发作持续至少2周以上、长者甚或数年，多数病例有反复发作的倾向，每次发作大多数可以缓解，部分可有残留症状或转为慢性。</p>-->
         </div>
-
       </div>
-      <div v-if="showWhich === 'ability'">ability</div>
-      <div v-if="showWhich === 'attitude'">attitude</div>
-      <div v-if="showWhich === 'character'">character</div>
-      <div v-if="showWhich === 'career'">career</div>
-      <div v-if="showWhich === 'decision'">decision</div>
-      <div v-if="showWhich === 'attainment'">attainment</div>
-      <div v-if="showWhich === 'comprehensive'">comprehensive</div>
+      <div v-if="showWhich === 'comprehensive'" class="one-item_all">comprehensive</div>
     </div>
 
   </div>
